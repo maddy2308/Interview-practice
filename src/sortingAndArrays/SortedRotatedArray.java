c2413bc44696a15ecb901d396e807d251a490656
maddy2308 <madhur.eng@gmail.com>
2015-04-27 21:47:12 -0400

    SortedRotatedArray had a missing condition to find the pivot

--- src/sortingAndArrays/SortedRotatedArray.java
+++ src/sortingAndArrays/SortedRotatedArray.java
@@ -3,10 +3,11 @@ package sortingAndArrays;
 
 public class SortedRotatedArray {
 
-    private int[] array = {3, 4, 5, 6, 7, 1, 2};
+    private int[] array = {7, 1, 2, 3, 4, 5, 6};//{3, 4, 5, 6, 7, 1, 2};
     private int keyToSearch;
-    public static void main(String[] args){
-        SortedRotatedArray object = new SortedRotatedArray(4);
+
+    public static void main(String[] args) {
+        SortedRotatedArray object = new SortedRotatedArray(7);
         int first = 0;
         int last = object.array.length - 1;
         int pivot = object.findPivot(first, last);
@@ -20,7 +21,7 @@ public class SortedRotatedArray {
 
     private int findKey(int first, int pivot, int last) {
         if (array[pivot] == keyToSearch) return pivot;
-        else if (array[first] <= keyToSearch && keyToSearch < array[pivot]){
+        else if (array[first] <= keyToSearch && keyToSearch < array[pivot]) {
             return findKey(first, pivot);
         } else {
             return findKey(pivot + 1, last);
@@ -28,27 +29,28 @@ public class SortedRotatedArray {
     }
 
     // assuming the array is sorted in ascending manner
-    public int findPivot(int first, int last){
-        int mid = (first + last)/2;
-        if (array[first] > array[last]){
+    public int findPivot(int first, int last) {
+        int mid = (first + last) / 2;
+        if (array[first] > array[last]) {
             // array is rotated
-            if (array[mid] > array[last]){
-                return findPivot(mid+1, last);
+            if (array[mid] > array[last]) {
+                return findPivot(mid + 1, last);
+            } else {
+                return findPivot(first, mid - 1);
             }
         }
         return first;
     }
 
-    private int findKey(int first, int last){
-        int mid = (first + last)/2;
-        if (first > last){
+    private int findKey(int first, int last) {
+        int mid = (first + last) / 2;
+        if (first > last) {
             return -1;
-        }
-        else if ((first == last) || (array[mid] == keyToSearch))
+        } else if ((first == last) || (array[mid] == keyToSearch))
             return mid;
-        else if (array[mid] > keyToSearch){
+        else if (array[mid] > keyToSearch) {
             return findKey(first, mid - 1);
-        }else {
+        } else {
             return findKey(mid + 1, last);
         }
     }
