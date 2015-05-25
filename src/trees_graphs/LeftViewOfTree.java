3711da94e2da74cda8e5969902e53517ecae9732
maddy2308 <madhur.eng@gmail.com>
2015-05-25 17:02:32 -0400

    added code for Left View of Tree

--- src/trees_graphs/LeftViewOfTree.java
+++ src/trees_graphs/LeftViewOfTree.java
@@ -18,5 +18,27 @@ public class LeftViewOfTree {
 
     public static void printLeftViewOfTree(TreeNode root) {
 
+        queue.add(root);
+        int previousLevel = 1, currentLevel = 0;
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            sb.append(temp.getData());
+            previousLevel--;
+            if (temp.getLeftChildNode() != null) {
+                queue.add(temp.getLeftChildNode());
+                currentLevel++;
+            }
+            if (temp.getRightChildNode() != null) {
+                queue.add(temp.getRightChildNode());
+                currentLevel++;
+            }
+            if (previousLevel == 0) {
+                System.out.println(sb.charAt(0));
+                previousLevel = currentLevel;
+                currentLevel = 0;
+                sb = new StringBuilder();
+            }
+        }
     }
 }
