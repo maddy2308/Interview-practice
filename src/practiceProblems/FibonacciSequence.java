2ef1e3e63654266b819350f90495597c008add7b
maddy2308 <madhur.eng@gmail.com>
2015-05-25 08:03:40 -0400

    added the formula used to calculate the fibonacci sequence using Golden ratio method

--- src/practiceProblems/FibonacciSequence.java
+++ src/practiceProblems/FibonacciSequence.java
@@ -6,6 +6,16 @@ import java.math.BigInteger;
 public class FibonacciSequence {
 
     // Using Golden Ratio to calculate the Fibonacci Sequence
+    // The formula is :-
+    /*
+        number = ((ratio ^ n) - ((1 - ratio) ^ n)) / (Sqrt(5))
+
+        where:
+            ratio  = 1.61815754
+            n = nth fibonacci number
+     */
+
+
     private static BigInteger getFibonacciNumber(int n) {
         double goldenRatio = 1.61815754;
         BigDecimal number = new BigDecimal((Math.pow(goldenRatio, n) - Math.pow((1 - goldenRatio), n)) / Math.sqrt(5));
